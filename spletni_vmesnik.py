1f8d63f81b47ce39423490e03f6e9d8619da34b3
ulab <ub2972@student.uni-lj.si>
2025-02-06 15:06:40 +0100

    sprememba vmesnik

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,5 +1,9 @@
 import bottle
 import sqlite3
+import os
+
+# Nastavimo pot do predlog na isti direktorij kot datoteka spletni_vmesnik.py
+bottle.TEMPLATE_PATH.insert(0, os.path.dirname(__file__))
 
 @bottle.get("/")
 def index():
@@ -18,22 +22,42 @@ def leta():
 def prvenstvo(leto):
     conn = sqlite3.connect("baza.sqlite")
 
-    # Pridobi podatke o tekmah
-    tekme_query = "SELECT datum, domaca_ekipa, domaci_goli, gostujoca_ekipa, gostujoci_goli FROM prvenstva WHERE leto = ?"
+    # Posodobljena poizvedba za pridobitev dodatnih podatkov
+    tekme_query = """
+    SELECT datum, domaca_ekipa, domaci_goli, gostujoca_ekipa, gostujoci_goli, 
+           stadion, mesto, stevilo_gledalcev
+    FROM prvenstva WHERE leto = ?
+    """
     tekme = [
-        {"datum": row[0], "domaca_ekipa": row[1], "domaci_goli": row[2], "gostujoca_ekipa": row[3], "gostujoci_goli": row[4]}
+        {
+            "datum": row[0],
+            "domaca_ekipa": row[1],
+            "domaci_goli": row[2],
+            "gostujoca_ekipa": row[3],
+            "gostujoci_goli": row[4],
+            "stadion": row[5],
+            "mesto": row[6],
+            "stevilo_gledalcev": row[7]
+        }
         for row in conn.execute(tekme_query, (leto,))
     ]
 
     # Pridobi podatke o igralcih
-    igralci_query = "SELECT ime_priimek, drzava, pozicija FROM igralec WHERE leto = ?"
+    igralci_query = """
+    SELECT ime_priimek, drzava, pozicija 
+    FROM igralec WHERE leto = ?
+    """
     igralci = [
-        {"ime_priimek": row[0], "drzava": row[1], "pozicija": row[2]}
+        {
+            "ime_priimek": row[0],
+            "drzava": row[1],
+            "pozicija": row[2],
+        }
         for row in conn.execute(igralci_query, (leto,))
     ]
 
     conn.close()
-    return bottle.json_dumps({"tekme": tekme, "igralci": igralci})
+    return bottle.template("prvenstvo.html", leto=leto, tekme=tekme, igralci=igralci)
 
 if __name__ == "__main__":
     bottle.run(host="localhost", port=8080, debug=True, reloader=True)
