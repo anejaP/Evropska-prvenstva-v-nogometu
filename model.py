eb2f3edd3223b6127543e6ab465d4ece31d21d0d
ulab <ub2972@student.uni-lj.si>
2025-02-07 14:30:22 +0100

    model

--- model.py
+++ model.py
@@ -1,7 +1,5 @@
 import sqlite3
 
-conn = sqlite3.connect('baza.sqlite')
-
 class Prvenstva:
     def __init__(self, id, datum, domaca_ekipa, gostujoca_ekipa, domaci_goli, gostujoci_goli, del_prvenstva, dodatek):
         self.id = id
@@ -12,35 +10,28 @@ class Prvenstva:
         self.gostujoci_goli = gostujoci_goli
         self.del_prvenstva = del_prvenstva
         self.dodatek = dodatek
-    
+
     def __str__(self):
         return f"{self.datum}, {self.domaca_ekipa} {self.domaci_goli} : {self.gostujoca_ekipa} {self.gostujoci_goli}, {self.del_prvenstva}, {self.dodatek}"
-    
-    def __repr__(self):
-        return f"Prvenstva({self.id}, {self.datum}, {self.domaca_ekipa}, {self.gostujoca_ekipa}, {self.domaci_goli}, {self.gostujoci_goli}, {self.del_prvenstva}, {self.dodatek})"
-    
+
     @staticmethod
     def prvenstvo(prvenstvo):
-        poizvedba = """SELECT * FROM prvenstva WHERE leto = ?"""
-        for row in conn.execute(poizvedba, [prvenstvo.leto]):
-            yield Prvenstva(*row)
+        poizvedba = "SELECT * FROM prvenstva WHERE leto = ?"
+        with sqlite3.connect('baza.sqlite') as conn:
+            rezultat = conn.execute(poizvedba, (prvenstvo,))
+            return [Prvenstva(*vrsta) for vrsta in rezultat.fetchall()]
 
-class Igralci:
-    def __init__(self, id, ime_priimek, drzava, rojstni_datum, pozicija):
-        self.id = id
+class Igralec:
+    def __init__(self, ime_priimek, pozicija):
         self.ime_priimek = ime_priimek
-        self.drzava = drzava
-        self.rojstni_datum = rojstni_datum
         self.pozicija = pozicija
 
     def __str__(self):
-        return f"{self.ime_priimek}, {self.drzava}, {self.rojstni_datum}, {self.pozicija}"
-    
-    def __repr__(self):
-        return f"Igralci({self.id}, {self.ime_priimek}, {self.drzava}, {self.rojstni_datum}, {self.pozicija})"
-    
-    @staticmethod
-    def igralec(igralec):
-        poizvedba = """SELECT * FROM igralec WHERE leto = ?"""
-        for row in conn.execute(poizvedba, [igralec.leto]):
-            yield Igralci(*row)
+        return f"{self.ime_priimek} (Pozicija: {self.pozicija})"
+
+def igralci_drzave_in_leto(drzava, leto):
+    conn = sqlite3.connect("baza.sqlite")
+    query = "SELECT ime_priimek, pozicija FROM igralec WHERE drzava = ? AND leto = ?"
+    igralci = conn.execute(query, (drzava, leto)).fetchall()
+    conn.close()
+    return [Igralec(row[0], row[1]) for row in igralci]
